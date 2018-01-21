41b9c7da54b54b2034d688e1b3824757bff2216c
shishir99111 <shishir99111@gmail.com>
2018-01-22 01:21:46 +0530

    minor changes

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const path = require('path');
-const Web3 = require('web3');
 const fs = require('fs');
 const co = require('co');
+const Web3 = require('web3');
 const solc = require('solc');
 
 require('dotenv').config({ path: path.join(__dirname, '/.env') });
@@ -14,15 +14,19 @@ async function contractDeployent() {
     const contestant = process.env.contestant.split(',');
     compiledCode = await solc.compile(code);
     const abiDefinition = JSON.parse(compiledCode.contracts[':Voting'].interface);
+    console.log(abiDefinition);
     const VotingContract = await web3.eth.contract(abiDefinition);
     const byteCode = compiledCode.contracts[':Voting'].bytecode;
     const deployedContract = await VotingContract.new(contestant, { data: byteCode, from: web3.eth.accounts[0], gas: 4700000 })
-    return VotingContract.at(deployedContract.address);
+    return deployedContract;
   } catch (e) {
     throw e;
   }
 }
 
-co(contractDeployent).then((contractAddress) => {
-  console.log('contractAddress', contractAddress);
-})
+co(contractDeployent).then((deployedContract) => {
+  const contractInstance = VotingContract.at(deployedContract);
+  console.log(`Contract is Deployed at ${deployedContract.address}`);
+}).catch((e) => {
+  throw e;
+});
